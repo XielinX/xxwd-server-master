fb975f9c2aff553eb6580a7c648fd37e23797282
mamgo <420923119@qq.com>
2019-07-01 20:38:53 +0800

    initial data

---
+++ src/main/java/com/xlx/xxwdservermaster/XxwdServerMasterApplication.java
@@ -0,0 +1,13 @@
+package com.xlx.xxwdservermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxwdServerMasterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XxwdServerMasterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/xlx/xxwdservermaster/XxwdServerMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xlx.xxwdservermaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XxwdServerMasterApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
